ec645b0d9efd89b4ca9e01f3597c3671821c7374
Ichinose Shogo <shogo82148@gmail.com>
2016-02-09 18:13:38 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Redis::Script - It's new $module
+
+# SYNOPSIS
+
+    use Redis::Script;
+
+# DESCRIPTION
+
+Redis::Script is ...
+
+# LICENSE
+
+Copyright (C) Ichinose Shogo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ichinose Shogo <shogo82148@gmail.com>
---
+++ lib/Redis/Script.pm
@@ -0,0 +1,39 @@
+package Redis::Script;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Redis::Script - It's new $module
+
+=head1 SYNOPSIS
+
+    use Redis::Script;
+
+=head1 DESCRIPTION
+
+Redis::Script is ...
+
+=head1 LICENSE
+
+Copyright (C) Ichinose Shogo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ichinose Shogo E<lt>shogo82148@gmail.comE<gt>
+
+=cut
+
