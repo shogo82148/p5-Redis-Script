abb417797bee8223703426449a2f31c014fd4381
Ichinose Shogo <shogo82148@gmail.com>
2022-05-06 07:32:50 +0900

    Thanks and Good bye TravisCI

--- lib/Redis/Script.pm
+++ lib/Redis/Script.pm
@@ -131,14 +131,14 @@ L<Description of EVAL|http://redis.io/commands/eval#bandwidth-and-evalsha>
 
 =head1 LICENSE
 
-Copyright (C) Ichinose Shogo.
+Copyright (C) ICHINOSE Shogo.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-Ichinose Shogo E<lt>shogo82148@gmail.comE<gt>
+ICHINOSE Shogo E<lt>shogo82148@gmail.comE<gt>
 
 =cut
 
